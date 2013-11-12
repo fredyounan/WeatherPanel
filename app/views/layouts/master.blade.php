2be52cebbd27d2360be03b11fea7dbf820575809
EliasZ <mail@eliaszerrouq.nl>
2013-11-12 14:11:16 +0100

    Fixes in navigatie

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -33,7 +33,7 @@
                 </div>
                 <div class="navbar-collapse collapse">
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Dashboard</a></li>
+                        <li class="active"><a href="{{ URL::to('/') }}">Dashboard</a></li>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Data <b class="caret"></b></a>
                             <ul class="dropdown-menu">
