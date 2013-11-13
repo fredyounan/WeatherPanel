83811308abf526e5af2b82288e1f2fd639da80d0
fabianPas <fabianp@fusionzone.nl>
2013-11-13 13:00:16 +0100

    swagg

---
+++ app/models/Parse.php
@@ -0,0 +1,21 @@
+<?php
+
+class Parse {
+
+	public function parseFile($file)
+	{
+		$result = array();
+	
+		$s = file_get_contents(public_path() . "/data/" . $file);
+		$arr = explode("\n", $s);
+		foreach($arr as $line)
+		{
+			if (!empty($line))
+			{
+				$result[] = explode(",", $line);
+			}
+		}
+		var_dump($result);
+	}
+	
+}
